ea5a990cb8d0689158d40912c3a128b0c54cee14
Osei Yeboah Johnson <johnyoat@gmail.com>
2016-10-16 20:19:30 +0100

    Add files via upload

---
+++ InputVerification.vb
@@ -0,0 +1,19 @@
+﻿Module InputVerification
+    Public Sub numsOnly(e As KeyPressEventArgs)
+        If Asc(e.KeyChar) <> 46 Then
+            If Asc(e.KeyChar) <> 8 Then
+                If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                    e.Handled = True
+                    MsgBox("Numbers Only", MsgBoxStyle.Information, "")
+                End If
+            End If
+        End If
+    End Sub
+    Public Sub lettersOnly(e As KeyPressEventArgs)
+        If AscW(e.KeyChar) > 64 And AscW(e.KeyChar) < 91 Or AscW(e.KeyChar) > 96 And AscW(e.KeyChar) < 123 Or AscW(e.KeyChar) = 8 Then
+        Else
+            e.KeyChar = Nothing
+            MsgBox("Letters Only", MsgBoxStyle.Exclamation, "")
+        End If
+    End Sub
+End Module
